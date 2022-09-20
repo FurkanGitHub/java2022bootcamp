767bc1e1e7dc0f8150a7b983a12ea3a52c84e60f
Furkan <f_154_i@hotmail.com>
2022-09-20 22:45:23 +0300

    Java2022bootcamp
    Java üzerinde yapılan kodlamalar.

---
+++ Java Kod 1. Hafta/DataTypes/src/dataTypes/VeriTipi.java
@@ -0,0 +1,14 @@
+package dataTypes;
+
+public class VeriTipi {
+
+	public static void main(String[] args) {
+	double sayi = 12.4;
+	sayi = -128;
+	
+	char karakter = 'A';
+	
+	boolean dogruMu = false ;
+	}
+
+}
